28e7bca158bc90e4a79eac13add234a773c16bb9
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2022-04-27 22:27:35 +0900

    Update RecyclerView2Activity.java

--- E03List/app/src/main/java/com/example/e03list/RecyclerView2Activity.java
+++ E03List/app/src/main/java/com/example/e03list/RecyclerView2Activity.java
@@ -1,13 +1,49 @@
 package com.example.e03list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class RecyclerView2Activity extends AppCompatActivity {
+
+    RecyclerView2Adapter recyclerView2Adapter;
+    ArrayList<Memo2> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view2);
+
+        arrayList = new ArrayList<Memo2>();
+        arrayList.add(new Memo2("one", new Date()));
+        arrayList.add(new Memo2("two", new Date()));
+
+        recyclerView2Adapter = new RecyclerView2Adapter(this, arrayList);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerView2Adapter);
+
+        Button b = (Button)findViewById(R.id.btnAdd);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText e = (EditText) findViewById(R.id.editText);
+                String s = e.getText().toString();
+                e.setText("");
+                arrayList.add(new Memo2(s, new Date()));
+                recyclerView2Adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
